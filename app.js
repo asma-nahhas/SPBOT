211d7d561138e33da008ed179d8c7aa96cf45045
unknown <asmanahasfree2@gmail.com>
2020-04-05 12:24:40 +0300

    last commit

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ function handlePostback(sender_psid, received_postback) {
       url: "https://graph.facebook.com/v2.6/" + sender_psid,
       qs: {
         access_token: PAGE_ACCESS_TOKEN,
-        fields: {"first_name","last_name","gender","age_range","location"}
+        fields:["first_name","last_name","gender","age_range","location"]
       },
       method: "GET"
     }, function(error, response, body) {
@@ -169,10 +169,16 @@ function handlePostback(sender_psid, received_postback) {
       } else {
         var bodyObj = JSON.parse(body);
 
-        console.log("Body Object last name: " +  bodyObj.last_name);
-        console.log("Body Object gender: " +  bodyObj.gender);
-        console.log("Body Object age: " +  bodyObj.age_range);
-        console.log("Body Object location: " +  bodyObj.location);
+      //  console.log("Body Object last name: " +  bodyObj.last_name);
+      //  console.log("Body Object gender: " +  bodyObj.gender);
+       // console.log("Body Object age: " +  bodyObj.age_range);
+        console.log("Body Object location: " +  body);
+        console.log("Body Object location: " +  bodyObj);
+        for (property in obj) {
+    output += property + ': ' + obj[property]+'; ';
+            console.log("Body Object location: " +  output);
+
+          }     
 
 
 
